20be38e72e5d72701c4243d56d4d15c9b62a09bc
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-04-26 15:15:43 -0300

    teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,15 +23,15 @@ import { UsersController } from './app/controllers/users.controller';
     AuthModule,
     MoviesModule,
     TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_CONNECTION,
+      type: 'mysql',
       host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
+      port: Number(process.env.TYPEORM_PORT),
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
       entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       synchronize: true,
-    } as TypeOrmModuleOptions),
+    }),
     
   
   ],
