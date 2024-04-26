2021d5440bbf4642df1779d253879089339070ef
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-04-26 15:32:09 -0300

    test

--- dist/app/typeorm.config.js
+++ dist/app/typeorm.config.js
@@ -5,11 +5,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 const config = {
     type: 'mysql',
-    host: process.env.TYPEORM_HOST,
-    port: Number(process.env.TYPEORM_PORT),
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE,
+    host: 'srv887.hstgr.io',
+    port: 3306,
+    username: 'u379300444_herbert_carlos',
+    password: '41568106hB',
+    database: 'u379300444_database_herbe',
     entities: [(0, path_1.join)(__dirname, '**', '*.entity.{ts,js}')],
     synchronize: true,
 };
