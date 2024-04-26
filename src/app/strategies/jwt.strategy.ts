6f40184e4aca08ba852cb83dfc2e6df3cf1a2c20
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-04-26 04:53:41 -0300

    testando chave secreta

--- src/app/strategies/jwt.strategy.ts
+++ src/app/strategies/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'T7DmftqVq+H3LLn3olyLD8P5uBqvtRRjiSS3LgCqbPI=',
+      secretOrKey: 'process.env.JWT_SECRET_KEY',
     });
   }
 
